8de835fb7785930d4c876cf37314498102adff78
epo21@ad.econ.cam.ac.uk <epo21@ad.econ.cam.ac.uk>
2019-08-16 22:16:10 +0100

    Added C++ file and a R file containing a wrapper function. Added an example to the description.

---
+++ R/safeBart_parallel.R
@@ -0,0 +1,104 @@
+#' @title Parallel Safe-Bayesian Random Forest
+#'
+#' @description A parallelized implementation of the Safe-Bayesian Random Forest described by Quadrianto and Ghahramani (2015)
+#' @param lambda A real number between 0 and 1 that determines the splitting probability in the prior (which is used as the importance sampler of tree models). Quadrianto and Ghahramani (2015) recommend a value less than 0.5 .
+#' @param num_trees The number of trees to be sampled.
+#' @param seed The seed for random number generation.
+#' @param num_cats The number of possible values for the outcome variable.
+#' @param y The training data vector of outcomes. This must be a vector of integers between 1 and num_cats.
+#' @param original_datamat The original training data. Currently all variables must be continuous. The training data does not need to be transformed before being entered to this function.
+#' @param alpha_parameters Vector of prior parameters.
+#' @param beta_par The power to which the likelihood is to be raised. For BMA, set beta_par=1.
+#' @param original_datamat The original test data. This matrix must have the same number of columns (variables) as the training data. Currently all variables must be continuous. The test data does not need to be transformed before being entered to this function.
+#' @param ncores The number of cores to be used in parallelization.
+#' @return A matrix of probabilities with the number of rows equl to the number of test observations and the number of columns equal to the number of possible outcome categories.
+#' @useDynLib safeBart, .registration = TRUE
+#' @examples
+#' beta_par <- 0.5
+#'
+#' N <- 100
+#' p<- 5
+#' set.seed(100)
+#'
+#' epsilon <- rnorm(N)
+#'
+#' xcov <- matrix(runif(N*p), nrow=N)
+#'
+#' y <- sin(pi*xcov[,1]*xcov[,2]) + 20*(xcov[,3]-0.5)^2+10*xcov[,4]+5*xcov[,5]+epsilon
+#' # <- rep(1,N) + epsilon
+#'
+#' epsilontest <- rnorm(N)
+#'
+#' xcovtest <- matrix(runif(N*p), nrow=N)
+#' ytest <- sin(pi*xcovtest[,1]*xcovtest[,2]) + 20*(xcovtest[,3]-0.5)^2+10*xcovtest[,4]+5*xcovtest[,5]+epsilontest
+#' #ytest <- rep(1,N) + epsilontest
+#'
+#'
+#'
+#'
+#' Num_split_vars <- 10
+#'
+#' lambda <- 0.45
+#' Num_models <- 10000
+#' num_trees1 <- 5
+#'
+#' seed1 <- 42
+#' ncores <- 7
+#'
+#'
+#'
+#' examplepreds1 <- safeBart_parallel(seed1,
+#'                                    y, xcov,xcovtest,
+#'                                    lambda=0.45,
+#'                                    num_models=Num_models,
+#'                                    num_trees=num_trees1,
+#'                                    beta_par=beta_par,
+#'                                    ncores=ncores,
+#'                                    outsamppreds=1,
+#'                                    nu=3,
+#'                                    a=3,
+#'                                    sigquant=0.9)
+#'
+#' cbind(examplepreds1,ytest )
+#' @export
+
+safeBart_parallel <- function(seed,
+                              y, original_datamat,test_datamat,
+                              lambda=0.45,num_models=1000,num_trees=5,
+                              beta_par=1,
+                              ncores=1,
+                              outsamppreds=1,
+                              nu=3,
+                              a=3,
+                              sigquant=0.9){
+
+
+  sigma=sd(y)/(max(y)-min(y))
+  qchi = qchisq(1.0-sigquant,nu,1,0);
+  lambdaBART = (sigma*sigma*qchi)/nu;
+
+  if(is.vector(original_datamat) | is.factor(original_datamat)| is.data.frame(original_datamat)) original_datamat = as.matrix(original_datamat)
+  if(is.vector(test_datamat) | is.factor(test_datamat)| is.data.frame(test_datamat)) test_datamat = as.matrix(test_datamat)
+
+  if((!is.matrix(original_datamat))) stop("argument x.train must be a double matrix")
+  if((!is.matrix(test_datamat)) ) stop("argument x.test must be a double matrix")
+
+  if(nrow(original_datamat) != length(y)) stop("number of rows in x.train must equal length of y.train")
+  if((ncol(test_datamat)!=ncol(original_datamat))) stop("input x.test must have the same number of columns as x.train")
+
+  sBARToutput=sBART_onefunc_parallel(lambda,
+                                     num_models,
+                                     num_trees,
+                                     seed,
+                                     y,
+                                     original_datamat,
+                                     beta_par,
+                                     test_datamat,
+                                     ncores,
+                                     outsamppreds,
+                                     nu,
+                                     a,
+                                     lambdaBART)
+
+  sBARToutput
+}
--- README.md
+++ README.md
@@ -3,14 +3,19 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of safeBart is to ...
+The goal of safeBart is to to provide implementations of Bayesian Additive Regression Trees and Bayesian Causal Forest using importance sampling. This can be viewed as an importance sampling approach to BART-BMA (Hernandez et al. 2018). The data-independent sampling from the prior and use of the power likelihood follows the safe-Bayesian Random Forest method described by Quadrianto and Ghahramani (2015).
+
+Hernández, B., Raftery, A. E., Pennington, S. R., & Parnell, A. C. (2018). Bayesian additive regression trees using Bayesian model averaging. Statistics and computing, 28(4), 869-890.
+
+Quadrianto, N., & Ghahramani, Z. (2014). A very simple safe-Bayesian random forest. IEEE transactions on pattern analysis and machine intelligence, 37(6), 1297-1303.
 
 ## Installation
 
 You can install the released version of safeBart from [CRAN](https://CRAN.R-project.org) with:
 
 ``` r
-install.packages("safeBart")
+library(devtools)
+install_github("EoghanONeill/safeBart")
 ```
 
 ## Example
@@ -19,6 +24,51 @@ This is a basic example which shows you how to solve a common problem:
 
 ``` r
 library(safeBart)
-## basic example code
+beta_par <- 0.5
+
+N <- 100
+p<- 5
+set.seed(100)
+
+epsilon <- rnorm(N)
+
+xcov <- matrix(runif(N*p), nrow=N)
+
+y <- sin(pi*xcov[,1]*xcov[,2]) + 20*(xcov[,3]-0.5)^2+10*xcov[,4]+5*xcov[,5]+epsilon
+# <- rep(1,N) + epsilon
+
+epsilontest <- rnorm(N)
+
+xcovtest <- matrix(runif(N*p), nrow=N)
+ytest <- sin(pi*xcovtest[,1]*xcovtest[,2]) + 20*(xcovtest[,3]-0.5)^2+10*xcovtest[,4]+5*xcovtest[,5]+epsilontest
+#ytest <- rep(1,N) + epsilontest
+
+
+
+
+Num_split_vars <- 10
+
+lambda <- 0.45
+Num_models <- 10000
+num_trees1 <- 5
+
+seed1 <- 42
+ncores <- 7
+
+
+
+examplepreds1 <- safeBart_parallel(seed1,
+  y, xcov,xcovtest,
+  lambda=0.45,
+  num_models=Num_models,
+  num_trees=num_trees1,
+  beta_par=beta_par,
+  ncores=ncores,
+  outsamppreds=1,
+  nu=3,
+  a=3,
+  sigquant=0.9)
+
+cbind(examplepreds1,ytest )
 ```
 
